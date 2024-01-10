eea142c7c39f097f96fd3a99e336ee3e8d70e61c
Helton Barbosa <heltonbs@gmail.com>
2024-01-10 15:48:41 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+number = int(input('Qual o número deseja calcular o fatorial: '))
+fatorial = 1
+
+for contador in range(1, number+1):
+    fatorial *= contador
+
+print(f'O fatorial de {number} é {fatorial}')
